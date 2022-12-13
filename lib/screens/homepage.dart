7f828cbb678b7842054ff3dfa5be249a42f91cda
Jocelyn Velarde <a01285780@itesm.mx>
2022-12-12 19:42:22 -0600

    added the first part of the main menu with several containers, also corresponding images

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,81 +4,143 @@ import 'package:smartshowerapp/screens/connectPage.dart';
 import 'package:smartshowerapp/screens/onboardingPage.dart';
 import 'package:smartshowerapp/screens/pointsPage.dart';
 import 'package:smartshowerapp/screens/settingsPage.dart';
-import 'package:smartshowerapp/widgets/button.dart';
-
-import '../main.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Color.fromARGB(137, 34, 34, 34),
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar.medium(
-                titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text(
-                    'Welcome to your Smart Shower',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
+        backgroundColor: const Color.fromARGB(137, 34, 34, 34),
+        appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/gradient.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            backgroundColor: Colors.transparent,
+            title: Text(
+              "Welcome to your Smart Shower!",
+            )),
+        body: SafeArea(
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.all(15),
+                width: 200,
+                height: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.black12,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://images.unsplash.com/photo-1607355739828-0bf365440db5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1444&q=80",
                     ),
-                    textAlign: TextAlign.right,
-                  ),
-                  background: Image.asset(
-                    'assets/gradient.jpg',
                     fit: BoxFit.cover,
                   ),
-
-                  /*decoration: const BoxDecoration(
-                    // LinearGradient
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-
-                      // colors for gradient
-                      colors: [
-                        Colors.deepPurpleAccent,
-                        Colors.blue,
-                      ],
-                    ),
-                  ),*/
                 ),
-                leading: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.menu),
-                ),
-                actions: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.search),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.more_vert),
-                  ),
-                ]),
-          ],
-        ),
-        /*Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 24),
-              ButtonWidget(
-                text: 'Go Back',
-                onClicked: () => goToOnBoarding(context),
-                key: Key('goBack'),
               ),
+              Container(
+                height: 50,
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 15,
+                      ),
+                      width: 60,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white30,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/musica.png",
+                          ),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      width: 60,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white30,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/corazon.png",
+                          ),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      width: 60,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/agua.png",
+                          ),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      width: 60,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white30,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/cuota.png",
+                          ),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        left: 20,
+                      ),
+                      width: 60,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white30,
+                        image: DecorationImage(
+                          image: AssetImage("assets/calendario.png"),
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
-        ),*/
+        ),
         bottomNavigationBar: GNav(
-          backgroundColor: Color.fromARGB(137, 34, 34, 34),
+          backgroundColor: const Color.fromARGB(137, 34, 34, 34),
           color: Colors.white,
-          activeColor: Color(0xFFB74093),
+          activeColor: const Color(0xFFB74093),
           tabs: [
             GButton(
                 icon: Icons.newspaper,
